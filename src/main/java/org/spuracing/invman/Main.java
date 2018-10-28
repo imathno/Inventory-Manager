79443b88f9f02f386c4ca067bdeb60d3ced0182f
imathno <imathno@hotmail.com>
2018-10-28 02:34:21 -0700

    Update GUI stuff

--- src/main/java/org/spuracing/invman/Main.java
+++ src/main/java/org/spuracing/invman/Main.java
@@ -13,16 +13,16 @@ import javafx.stage.Stage;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		DynamoDBManager manager = DynamoDBManager.getInstance();
-		manager.setTable(Constants.TABLE_NAME);
-		manager.addItemToTable("Wrench", 832089);
-		manager.addItemToTable("Jack Hammer", 432);
-		manager.addItemToTable("Saw", 542);
-		manager.addItemToTable("Screwdriver", 3254);
-		manager.addItemToTable("Nitrous", 38093);
-		//DynamoTableReader reader = new DynamoTableReader(DynamoDBManager.getInstance().getTable());
-		//System.out.println(reader.getItemQuantity(Constants.PRIMARY_KEY, "D"));
-		//LauncherImpl.launchApplication(InvManApplication.class, InvManPreloader.class, args);
+//		DynamoDBManager manager = DynamoDBManager.getInstance();
+//		manager.setTable(Constants.TABLE_NAME);
+//		manager.addItemToTable("Wrench", 832089);
+//		manager.addItemToTable("Jack Hammer", 432);
+//		manager.addItemToTable("Saw", 542);
+//		manager.addItemToTable("Screwdriver", 3254);
+//		manager.addItemToTable("Nitrous", 38093);
+//		DynamoTableReader reader = new DynamoTableReader(DynamoDBManager.getInstance().getTable());
+//		System.out.println(reader.getItemQuantity(Constants.PRIMARY_KEY, "D"));
+		LauncherImpl.launchApplication(InvManApplication.class, InvManPreloader.class, args);
 	}
 
 }
--- src/main/java/org/spuracing/invman/gui/InvManApplication.java
+++ src/main/java/org/spuracing/invman/gui/InvManApplication.java
@@ -3,9 +3,11 @@ package org.spuracing.invman.gui;
 import org.spuracing.invman.Constants;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class InvManApplication extends Application {
@@ -13,11 +15,17 @@ public class InvManApplication extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		stage.setTitle(Constants.GUI_TITLE);
-		BorderPane root = new BorderPane(new Label("Welcome!"));
-	    Scene scene = new Scene(root);
-	    stage.setWidth(800);
-	    stage.setHeight(600);
+		StackPane root = new StackPane();
+		Button update = new Button("Update");
+		TextField textBox = new TextField();
+		textBox.setPrefWidth(4);
+		root.getChildren().add(textBox);
+	    root.getChildren().add(update);
+	    StackPane.setAlignment(textBox, Pos.CENTER_LEFT);
+	    StackPane.setAlignment(update, Pos.CENTER_RIGHT);
+	    Scene scene = new Scene(root, 800, 600);
 	    stage.setScene(scene);
+	    
 		stage.show();
 	}
 
